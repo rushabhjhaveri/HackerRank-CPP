ad24eea036b583f00c14678b8b9e043c35602a25
Rushabh Jhaveri <rjhaveri98@gmail.com>
2017-11-11 10:05:39 -0500

    Added Pointer.

---
+++ Introduction Challenges/Pointer/pointer_update.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void update(int *a, int *b){
+	//Complete this function.
+	int ap = *a;
+	*a = *a + *b;
+	*b = abs(ap-*b);
+}
+
+int main(){
+	int a, b;
+	int *pa = &a, *pb = &b;
+	scanf("%d%d",&a,&b);
+	update(pa,pb);
+	printf("%d\n%d",a,b);
+	return 0;
+}
